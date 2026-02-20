a5256fb5f3ab0596f0ad5562d66d8fb4ad8ac2d7
nuyuls79 <nuyulshare001@gmail.com>
2026-02-20 10:55:33 +0700

    extract lima.zip

---
+++ CNC Verse/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
@@ -0,0 +1,22 @@
+package com.horis.cncverse
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+open class CNCVersePlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        NetflixMirrorStorage.init(context.applicationContext)
+        DisneyPlusProvider.context = context
+        NetflixMirrorProvider.context = context
+        PrimeVideoMirrorProvider.context = context
+        HotStarMirrorProvider.context = context
+        registerMainAPI(NetflixMirrorProvider())
+        registerMainAPI(PrimeVideoMirrorProvider())
+        registerMainAPI(HotStarMirrorProvider())
+        registerMainAPI(DisneyPlusProvider())
+    }
+
+}
