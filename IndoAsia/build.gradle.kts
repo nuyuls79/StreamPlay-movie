316f7fa0ac1dbd7c0e38c88d43860e5724949c19
nuyuls79 <nuyulshare001@gmail.com>
2026-05-16 12:01:08 +0700

    Add build.gradle.kts for IndoAsia project

---
+++ IndoAsia/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 7
+
+
+cloudstream {
+    language = "id"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Jangan Coli Yaa"
+    authors = listOf("aldry84")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+
+
+    iconUrl = "https://homecookingrocks.com/wp-content/uploads/2024/10/cropped-indomax21-favicon-color-1.png"
+
+}
